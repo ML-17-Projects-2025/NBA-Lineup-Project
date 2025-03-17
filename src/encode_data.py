854326ea49db9a0a7e82e19dc7e0029636028e7a
DemiwizAces <erdmriosnadeau@gmail.com>
2025-03-17 14:49:14 -0400

    Save uncommitted changes before LFS migration

--- src/encode_data.py
+++ src/encode_data.py
@@ -1,3 +1,26 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:88be1f205a1e727f04f7987672ecd3d9d365243e499413ea6f17d9b5b76bd97b
-size 946
+import pandas as pd
+import os
+from sklearn.preprocessing import LabelEncoder
+
+# Define the file paths for the datasets
+file_paths = [f"../datasets/original/matchups-{year}.csv" for year in range(2007, 2016)]
+encoded_folder = "../datasets/encoded"
+
+# Iterate through each file, read it, encode labels, and save the updated DataFrame
+for file in file_paths:
+    # Load the dataset
+    matchup_data = pd.read_csv(file)
+
+    # Encode categorical columns (players and teams) using LabelEncoder
+    label_encoders = {}
+    for col in ["home_0", "home_1", "home_2", "home_3", "home_4", "home_team", "away_team"]:
+        le = LabelEncoder()
+        matchup_data[col] = le.fit_transform(matchup_data[col])
+        label_encoders[col] = le  # Save encoders for later decoding
+
+    # Extract the file name from the path
+    file_name = os.path.basename(file)  
+    new_file_path = os.path.join(encoded_folder, file_name.replace(".csv", "-encoded.csv"))
+
+    # Save the updated DataFrame with encoded labels to a new file
+    matchup_data.to_csv(new_file_path, index=False)
